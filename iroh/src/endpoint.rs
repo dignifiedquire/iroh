f78b9054bfef041b3ee1f08b28d3297caab9a296
“ramfox” <kasey@n0.computer>
2025-01-08 17:07:00 -0500

    fix rebase mistake

--- iroh/src/endpoint.rs
+++ iroh/src/endpoint.rs
@@ -975,10 +975,6 @@ impl Endpoint {
             return;
         }
 
-        tracing::debug!("Closing connections");
-        self.endpoint.close(0u16.into(), b"");
-        self.endpoint.wait_idle().await;
-
         tracing::debug!("Connections closed");
         self.msock.close().await;
     }
